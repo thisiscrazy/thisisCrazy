aa54dffc5ab2b5c6b9a2d113cf41007a731bd9dd
3pehrdev <sepehrsalehi0111@gmail.com>
2016-03-28 16:23:45 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {85831686)},--Sudo users
+    sudo_users = {85831686},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Sphero-bot v1.2beta
