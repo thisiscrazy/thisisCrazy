dce6f2085a301d295e78336f5c9ebf354d2ec3d2
3pehrdev <sepehrsalehi0111@gmail.com>
2016-03-30 12:19:50 +0430

    Create feedback.lua

---
+++ plugins/feedback.lua
@@ -0,0 +1,21 @@
+do
+ function run(msg, matches)
+ local fuse = '#NewFeedback\n\nUsername: @''\n\nUserId : ' .. msg.from.id .. '\n\nGroupId : ' .. msg.to.id .. '\n\nName: ' .. msg.from.print_name .. '\n\nMessage:\n\n'  .. matches[1]
+ local fuses = '!printf user#id' .. msg.from.id
+ local text = matches[1]
+ local sends = send_msg('user#id85831686', fuse, ok_cb, false)
+ return 'Your Feedback Sucessfuly Send to @joker_admin_1!'
+ end
+end
+return {
+description = "Feedback",
+usage = "feedback message",
+patterns = {
+
+ "^[Ff]eedback (.*)$",
+ "^[!/][Ff]eedback (.*)$"
+ 
+
+ },
+ run = run
+}
