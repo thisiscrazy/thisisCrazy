59d33ccb96827c75a31f164e721156d43eafc412
3pehrdev <sepehrsalehi0111@gmail.com>
2016-03-30 11:56:42 +0430

    Create version.lua

---
+++ plugins/version.lua
@@ -0,0 +1,20 @@
+do
+
+function run(msg, matches)
+  return 'Telegram Bot '.. VERSION .. [[ 
+Sphero - bot
+version 1.2 beta
+managed and developed
+by : @joker_admin_1 .]]
+end
+
+return {
+  description = "Shows bot version", 
+  usage = "!version: Shows bot version",
+  patterns = {
+    "^!version$"
+  }, 
+  run = run 
+}
+
+end
