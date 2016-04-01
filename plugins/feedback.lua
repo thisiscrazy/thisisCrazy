c27086de6315d94ab648c22dea30067d84eecd7f
3pehrdev <sepehrsalehi0111@gmail.com>
2016-04-01 20:52:21 +0430

    Update feedback.lua

--- plugins/feedback.lua
+++ plugins/feedback.lua
@@ -3,8 +3,8 @@ do
  local fuse = '#NewFeedback\n\nUsername: @''\n\nUserId : ' .. msg.from.id .. '\n\nGroupId : ' .. msg.to.id .. '\n\nName: ' .. msg.from.print_name .. '\n\nMessage:\n\n'  .. matches[1]
  local fuses = '!printf user#id' .. msg.from.id
  local text = matches[1]
- local sends = send_msg('user#id85831686', fuse, ok_cb, false)
- return 'Your Feedback Sucessfuly Send to @joker_admin_1!'
+ local sends = send_msg('user#id147696943', fuse, ok_cb, false)
+ return 'Your Feedback Sucessfuly Send to Support Sphero!'
  end
 end
 return {
