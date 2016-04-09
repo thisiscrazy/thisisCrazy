f013a8fa98fced2dad993738ac4f385aff659e82
3pehrdev <sepehrsalehi0111@gmail.com>
2016-04-09 18:10:45 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -1,19 +1,19 @@
 do function run(msg, matches)
 
-    if matches[1]:lower() == 'id' and is_sudo(msg) then
-      return "Sudo🌐\n🔺Your id🔺\n"..msg.from.id.."\n🔺Your name🔺\n"..msg.from.print_name.."\n🔺Group name🔺\n"..msg.to.title.."\n🔺Group id🔺\n"..msg.to.id
+	  if matches[1]:lower() == 'id' and is_sudo(msg) then
+      return "Sudo☑\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Your name⭕️\n"..msg.from.print_name.."\n⭕️Group name⭕️\n"..msg.to.title.."\n⭕️Group id⭕️\n"..msg.to.id
       end
       if matches[1]:lower() == 'id' and is_admin(msg) then
-      return "Admin▫️\n🔺Your id🔺\n"..msg.from.id.."\n🔺Your name🔺\n"..msg.from.print_name.."\n🔺Group name\n"..msg.to.title.."\n🔺Group id🔺\n"..msg.to.id
+      return "Admin☑️\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Your name⭕️\n"..msg.from.print_name.."\n⭕️Group name⭕️\n"..msg.to.title.."\n⭕️Group id⭕️\n"..msg.to.id
       end
       if matches[1]:lower() == 'id' and is_owner(msg) then
-      return "Owner▫️\n🔺Your id🔺\n"..msg.from.id.."\n🔺Your name🔺\n"..msg.from.print_name.."\n🔺Group name🔺\n"..msg.to.title.."\n🔺Group id🔺\n"..msg.to.id
+      return "Owner☑️\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Your name⭕️\n"..msg.from.print_name.."\n⭕️Group name⭕️\n"..msg.to.title.."\n⭕️Group id⭕️\n"..msg.to.id
       end
       if matches[1]:lower() == 'id' and is_momod(msg) then
-      return "Moderator▫️\n🔺Your id🔺\n"..msg.from.id.."\n🔺Your name🔺\n"..msg.from.print_name.."\n🔺Group name🔺\n"..msg.to.title.."\n🔺Group id🔺\n"..msg.to.id
+      return "Moderator☑️\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Your name⭕️\n"..msg.from.print_name.."\n⭕️Group name⭕️\n"..msg.to.title.."\n⭕️Group id⭕️\n"..msg.to.id
       end
       if matches[1]:lower() == 'id' then
-      return "Member▫️\n🔺Your id🔺\n"..msg.from.id.."\n🔺Your name🔺\n"..msg.from.print_name.."\n🔺Group name🔺\n"..msg.to.title.."\n🔺Group id🔺\n"..msg.to.id
+      return "Member☑️\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Your name⭕️\n"..msg.from.print_name.."\n⭕️Group name⭕️\n"..msg.to.title.."\n⭕️Group id⭕️\n"..msg.to.id
     end
 end
   return {
@@ -26,4 +26,4 @@ end
 }
 end
 
-—sepehr blacklifeTM
+--sepehr blacklifeTM
