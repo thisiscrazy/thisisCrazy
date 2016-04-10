fd5ec564032b326788ca5ebd3652f801532175ac
3pehrdev <sepehrsalehi0111@gmail.com>
2016-04-10 17:45:11 +0430

    Update ingroup.lua

--- plugins/ingroup.lua
+++ plugins/ingroup.lua
@@ -235,7 +235,7 @@ local lock_sticker = "ok"
         lock_sticker = data[tostring(msg.to.id)]['settings']['sticker']
         end
          local settings = data[tostring(target)]['settings']
-  local text = "#Group settings:\n=======================\n#Lock group name : "..settings.lock_name.."\n=======================\n#Lock group photo : "..settings.lock_photo.."\n=======================\n#Lock group tag : "..lock_tag.."\n=======================\n#Lock group member : "..settings.lock_member.."\n=======================\n#Lock group english 🗣 : "..lock_eng.."\n=======================\n#Lock group leave : "..lock_leave.."\n=======================\n#Lock group badw : "..lock_badw.."\\n=======================\n#Lock group links : "..lock_link.."\n=======================\n#Lock group sticker : "..lock_sticker.."\\n=======================\n#flood number : "..NUM_MSG_MAX.."\n=======================\n#Lock group Bot : "..bots_protection--"\nPublic: "..public
+  local text = "#Group settings:\n#Lock group name : "..settings.lock_name.."\n#Lock group photo : "..settings.lock_photo.."\n#Lock group tag : "..lock_tag.."\n#Lock group member : "..settings.lock_member.."\n#Lock group english 🗣 : "..lock_eng.."\n#Lock group leave : "..lock_leave.."\n#Lock group badw : "..lock_badw.."\n#Lock group links : "..lock_link.."\n#Lock group sticker : "..lock_sticker.."\n#flood number : "..NUM_MSG_MAX.."\n#Lock group Bot : "..bots_protection--"\nPublic: "..public
   return text
 end
 
