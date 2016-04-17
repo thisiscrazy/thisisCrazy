a921438f145ad3788125636ac685b35e50feed59
3pehrdev <sepehrsalehi0111@gmail.com>
2016-04-17 12:43:28 +0430

    Create infoes.lua

---
+++ plugins/infoes.lua
@@ -0,0 +1,29 @@
+do
+
+function run(msg, matches)
+local reply_id = msg['id']
+
+local info = '🔰FirstName : '..msg.from.first_name..'\n\n'
+..'🔰Lastname : '..msg.from.last_name..'\n
+..'🔰Fullname : '..msg.from.print_name..'\n
+..'🔰Id : '..msg.from.id..'\n'
+..'🔰Username : @'..msg.from.username..'\n'
+..'🔰Msg ID : '..msg.id..'\n'
+..'🔰Your Msg : '..msg.text..'\n\n'
+..'🔰Group Id : '..msg.to.id..'\n'
+..'🔰Group name : '..msg.to.title..'\n'
+..'🔰 Your phone : +'..msg.from.phone..'\n'
+..'🔰Your Name :  '..msg.from.first_name..'\nby @Joker_Admin_1 :)'
+
+reply_msg(reply_id, info, ok_cb, false)
+end
+
+return {
+patterns = {
+"^[!/#]info"
+
+},
+run = run
+}
+
+end
