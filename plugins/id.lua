0d97736bf71a7e1378de0af04430b6c387dc6570
3pehrdev <sepehrsalehi0111@gmail.com>
2016-04-18 21:06:07 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -1,29 +1,26 @@
 do function run(msg, matches)
-
-	  if matches[1]:lower() == 'id' and is_sudo(msg) then
-      return "Sudo☑\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Your name⭕️\n"..msg.from.print_name.."\n⭕️Group name⭕️\n"..msg.to.title.."\n⭕️Group ID⭕️\n"..msg.to.id.."\nmsg ID\n"..msg.id.."\nYour msg\n"..msg.text.."\nUsername\n"..msg.from.username
-      end
-      if matches[1]:lower() == 'id' and is_admin(msg) then
-      return "Admin☑️\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Your name⭕️\n"..msg.from.print_name.."\n⭕️Group name⭕️\n"..msg.to.title.."\n⭕️Group id⭕️\n"..msg.to.id
-      end
-      if matches[1]:lower() == 'id' and is_owner(msg) then
-      return "Owner☑️\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Your name⭕️\n"..msg.from.print_name.."\n⭕️Group name⭕️\n"..msg.to.title.."\n⭕️Group id⭕️\n"..msg.to.id
-      end
-      if matches[1]:lower() == 'id' and is_momod(msg) then
-      return "Moderator☑️\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Your name⭕️\n"..msg.from.print_name.."\n⭕️Group name⭕️\n"..msg.to.title.."\n⭕️Group id⭕️\n"..msg.to.id
-      end
-      if matches[1]:lower() == 'id' then
-      return "Member☑️\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Your name⭕️\n"..msg.from.print_name.."\n⭕️Group name⭕️\n"..msg.to.title.."\n⭕️Group id⭕️\n"..msg.to.id
-    end
+if matches[1]:lower() == 'id' and is_sudo(msg) then
+return "Sudo☑️\n#Your id\n"..msg.from.id.."\n#Your name\n"..msg.from.print_name.."\n#Group name\n"..msg.to.title.."\n#Group ID\n"..msg.to.id.."\n#msg ID\n"..msg.id.."\n#Your msg\n"..msg.text.."\n#Username\n@"..msg.from.username
 end
-  return {
-  description = "show your id",
-  usage = "me",
-  patterns = {
-  "^[!/#]([Ii][Dd])$",
-    },
-  run = run
+if matches[1]:lower() == 'id' and is_admin(msg) then
+return "Admin☑️\n#Your id\n"..msg.from.id.."\n#Your name\n"..msg.from.print_name.."\n#Group name\n"..msg.to.title.."\n#Group ID\n"..msg.to.id.."\n#msg ID\n"..msg.id.."\n#Your msg\n"..msg.text.."\n#Username\n@"..msg.from.username
+end
+if matches[1]:lower() == 'id' and is_owner(msg) then
+return "Owner☑️\n#Your id\n"..msg.from.id.."\n#Your name\n"..msg.from.print_name.."\n#Group name\n"..msg.to.title.."\n#Group ID\n"..msg.to.id.."\n#msg ID\n"..msg.id.."\n#Your msg\n"..msg.text.."\n#Username\n@"..msg.from.username
+end
+if matches[1]:lower() == 'id' and is_momod(msg) then
+return "Moderator☑️\n#Your id\n"..msg.from.id.."\n#Your name\n"..msg.from.print_name.."\n#Group name\n"..msg.to.title.."\n#Group ID\n"..msg.to.id.."\n#msg ID\n"..msg.id.."\n#Your msg\n"..msg.text.."\n#Username\n@"..msg.from.username
+end
+if matches[1]:lower() == 'id' then
+return "Member☑️\n#Your id\n"..msg.from.id.."\n#Your name\n"..msg.from.print_name.."\n#Group name\n"..msg.to.title.."\n#Group ID\n"..msg.to.id.."\n#msg ID\n"..msg.id.."\n#Your msg\n"..msg.text.."\n#Username\n@"..msg.from.username
+end
+end
+return {
+description = "show your id",
+usage = "me",
+patterns = {
+"^[!/#]([Ii][Dd])$",
+},
+run = run
 }
 end
-
---sepehr blacklifeTM
