b371b72119382a43bf3588e6942b7019b51b361d
3pehrdev <sepehrsalehi0111@gmail.com>
2016-04-18 20:57:08 +0430

    Update id.lua

--- plugins/id.lua
+++ plugins/id.lua
@@ -1,7 +1,7 @@
 do function run(msg, matches)
 
 	  if matches[1]:lower() == 'id' and is_sudo(msg) then
-      return "Sudo☑\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Your name⭕️\n"..msg.from.print_name.."\n⭕️Group name⭕️\n"..msg.to.title.."\n⭕️Group id⭕️\n"..msg.to.id
+      return "Sudo☑\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Your name⭕️\n"..msg.from.print_name.."\n⭕️Group name⭕️\n"..msg.to.title.."\n⭕️Group ID⭕️\n"..msg.to.id.."\nmsg ID\n"..msg.id.."\nYour msg\n"..msg.text.."\nUsername\n"..msg.from.username
       end
       if matches[1]:lower() == 'id' and is_admin(msg) then
       return "Admin☑️\n⭕️Your id⭕️\n"..msg.from.id.."\n⭕️Your name⭕️\n"..msg.from.print_name.."\n⭕️Group name⭕️\n"..msg.to.title.."\n⭕️Group id⭕️\n"..msg.to.id
