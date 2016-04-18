406d461f6312a64ddefeb4efb9925b855c9d9381
3pehrdev <sepehrsalehi0111@gmail.com>
2016-04-18 21:33:37 +0430

    Update ingroup.lua

--- plugins/ingroup.lua
+++ plugins/ingroup.lua
@@ -1524,7 +1524,7 @@ if matches[1] == 'newlink' and not is_realm(msg) then
         return "no owner,ask admins in support groups to set owner for your group"
       end
       savelog(msg.to.id, name_log.." ["..msg.from.id.."] used /owner")
-      return "#Group owner : "Group onwer is @"..user_info.username.." ["..group_owner.."]"
+      return "#Group owner : "@"..user_info.username.." ["..group_owner.."]"
     end
     if matches[1] == 'setgpowner' then
       local receiver = "chat#id"..matches[2]
