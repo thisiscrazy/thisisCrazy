eeb2159ef0bb34449c4938ba8022ec703e2b5a86
3pehrdev <sepehrsalehi0111@gmail.com>
2016-04-21 22:26:06 +0430

    Update expirechat.lua

--- plugins/expirechat.lua
+++ plugins/expirechat.lua
@@ -1,12 +1,14 @@
-function run(msg,matches)
- if matches[1]:lower() == 'GroupTimeEnd!:free' and is_admin(msg) then
- return "!rem"
+local function run(msg)
+if msg.text == "GroupTimeEnd!:free" then 
+return "!rem"
 end
-return {
-  description = " spammer ", 
-  patterns = {
-    "^GroupTimeEnd!:free$",
-  },
-  run = run
+end 
+return { 
+description = "Chat With Robot Server", 
+usage = "chat with robot",
+ patterns = {
+ "^GroupTimeEnd!:free$",
+},
+ run = run, 
+pre_process = pre_process
 }
-end
