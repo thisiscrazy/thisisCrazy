45d8bfa82190302c8a763d31a65a882f420fd112
3pehrdev <sepehrsalehi0111@gmail.com>
2016-04-23 00:47:45 +0430

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -69,7 +69,7 @@ function msg_valid(msg)
 
   if not msg.from.id then
     print('\27[36mNot valid: From id not provided\27[39m')
-    return true
+    return false
   end
 
   if msg.from.id == our_id then
