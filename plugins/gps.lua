646ca75e98edf11de27e79ca41e39913f52a0fc1
3pehrdev <sepehrsalehi0111@gmail.com>
2016-05-03 19:32:13 +0430

    Update gps.lua

--- plugins/gps.lua
+++ plugins/gps.lua
@@ -21,6 +21,7 @@ end
 return {
   description = "generates a map showing the given GPS coordinates", 
   usage = "!gps latitude,longitude: generates a map showing the given GPS coordinates",
+},
   patterns = {"^!gps ([^,]*)[,%s]([^,]*)$"}, 
   run = run 
 }
